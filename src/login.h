203598e1a30859b29c0995a945cb0bbfc1cd9ffa
Kerigan <kerigancreighton@gmail.com>
2024-05-22 22:44:36 -0500

    configure for fingerprint, set rainbow theme as default

--- src/login.h
+++ src/login.h
@@ -4,10 +4,17 @@
 #include "draw.h"
 #include "inputs.h"
 
+bool fingerprint_auth(struct term_buf* buf, const char* username);
+
 void auth(
 	struct desktop* desktop,
 	struct text* login,
 	struct text* password,
 	struct term_buf* buf);
+
+void post_auth(
+    struct desktop* desktop,
+    struct text* login,
+    struct term_buf* buf);
 
 #endif
